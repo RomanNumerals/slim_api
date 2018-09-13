0d29db35453f2f97a30338494562b02049282c10
Rafael Roman <31549567+RomanNumerals@users.noreply.github.com>
2018-09-13 09:21:05 -0400

    adding missing elements based on reference point

---
+++ public/slimApi/app.php
@@ -0,0 +1,161 @@
+<?php
+namespace roman\slimApi;
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+require './vendor/autoload.php';
+
+class App {
+
+	private $app;
+	public function __construct($db) {
+
+		$config['db']['host'] = 'localhost';
+		$config['db']['user'] = 'root';
+		$config['db']['pass'] = 'test';
+		$config['db']['dbname'] = 'apiDB';
+
+		$app = new \Slim\App(['settings' => $config]);
+
+		$container = $app->getContainer();
+		$container['db'] = $db;
+
+		$container['logger'] = function($c) {
+			$logger = new \Monolog\Logger('my_logger');
+			$file_handler = new
+			\Monolog\Handler\StreamHandler('./logs/app.log');
+			$logger->pushHandler($file_handler);
+			return $logger;
+		};
+
+		$app->get('/fruit', function(Request $request, Response $response) {
+			$this->logger->addInfo("GET /fruit");
+			$fruit = $this->db->query('SELECT * FROM fruit')->fetchAll();
+			$jsonResponse = $response->withJson($fruit);
+			return $jsonResponse;
+		});
+
+		$app->get('/fruit/{id}', function(Request $request, Response $response, array $args) {
+			$id = $args['id'];
+			$this->logger->addInfo("GET /fruit/".$id);
+			$fruit = $this->db->query('SELECT * FROM fruit WHERE id='.$id)->fetch();
+
+			if($fruit) {
+				$response = $response ->withJson($fruit);
+			} else {
+				$errorData = array('status' => 404, 'message' => 'not found');
+				$response = $response->withJson($errorData, 404);
+			}
+			return $response;
+		});
+
+		$app->post('/fruit', function(Request $request, Response $response) {
+			$this->logger->addInfo("POST /people/");
+
+			/*
+			- Check that fruit exists
+			- $person = $this->db->query('SELECT * FROM fruit WHERE id='.$id)->fetch();
+			- if(!$fruit) {
+			$errorData = array('status' => 404, 'message' => 'not found');
+			- $response = $response->withJson($errorData, 404);
+			- return $response;
+		}
+			*/
+
+			# build query string
+			$createString = "INSERT INTO fruit ";
+			$fields = $request->getParsedBody();
+			$keysArray = array_keys($fields);
+			$last_key = end($keysArray);
+			$values = '(';
+			$fieldNames = '(';
+			foreach($fields as $field => $value) {
+				$values = $values . "'" . "$value" . "'";
+				$fieldNames = $fieldNames . "$field";
+				if ($field != $last_key) {
+					# conditionally add a comma to avoid sql syntax problems
+					$values = $values . ", ";
+					$fieldNames = $fieldNames . ", ";
+				}
+			}
+			$values = $values . ')';
+			$fieldNames = $fieldNames . ') VALUES ';
+			$createString = $createString . $fieldNames . $values . ";";
+			# execute query
+			try {
+				$this->db->exec($createString);
+			} catch (\PDOException $e) {
+				var_dump($e);
+				$errorData = array('status' => 400, 'message' => 'Invalid data provided to create fruit');
+				return $response->withJson($errorData, 400);
+			}
+			# return updated record
+			$fruit = $this->db->query('SELECT * FROM fruit ORDER BY id DESC LIMIT 1')->fetch();
+			$jsonResponse = $response->withJson($fruit);
+
+			return $jsonResponse;
+		});
+
+		$app->put('/fruit/{id}', function (Request $request, Response $response, array $args) {
+				$id = $args['id'];
+				$this->logger->addInfo("PUT /fruit/".$id);
+
+				// check that peron exists
+				$fruit = $this->db->query('SELECT * FROM fruit WHERE id='.$id)->fetch();
+				if(!$fruit){
+					$errorData = array('status' => 404, 'message' => 'not found');
+					$response = $response->withJson($errorData, 404);
+					return $response;
+				}
+
+				// build query string
+				$updateString = "UPDATE fruit SET ";
+				$fields = $request->getParsedBody();
+				$keysArray = array_keys($fields);
+				$last_key = end($keysArray);
+				foreach($fields as $field => $value) {
+					$updateString = $updateString . "$field = '$value'";
+					if ($field != $last_key) {
+						// conditionally add a comma to avoid sql syntax problems
+						$updateString = $updateString . ", ";
+					}
+				}
+				$updateString = $updateString . " WHERE id = $id;";
+
+				// execute query
+				try {
+					$this->db->exec($updateString);
+				} catch (\PDOException $e) {
+					$errorData = array('status' => 400, 'message' => 'Invalid data provided to update');
+					return $response->withJson($errorData, 400);
+				}
+				// return updated record
+				$person = $this->db->query('SELECT * FROM fruit WHERE id='.$id)->fetch();
+				$jsonResponse = $response->withJson($fruit);
+
+				return $jsonResponse;
+		});
+
+		$app->delete('/fruit{id}', function(Request $request, Response $response, array $args) {
+			$id = $args['id'];
+			$this->logger->addInfo("DELETE /fruit/".$id);
+			$deleteMsg = $this->db->exec('DELETE FROM fruit WHERE id='.$id);
+			if($deleteMsg) {
+				$response = $response->withStatus(200);
+			} else {
+				$errorData = array('status' => 404, 'message' => 'not found');
+				$response = $response->withJson($errorData, 404);
+			}
+			return $response;
+		});
+
+		$this->app = $app;
+	}
+
+	/**
+	 * Get an instance of the app
+	 * @return \Slim\App
+	 */
+	 public function get() {
+	 	return $this->app;
+	 }
+}
--- public/slimApi/test/test.php
+++ public/slimApi/test/test.php
@@ -2,15 +2,19 @@
 use PHPUnit\Framework\TestCase;
 use Slim\Http\Environment;
 use Slim\Http\Request;
+use Slim\Http\Uri;
+use Slim\Http\RequestBody;
 require './vendor/autoload.php';
 
 # empty class definitions for phpunit to mock
 class mockQuery {
 	public function fetchAll(){}
+	public function fetch(){}
 };
 
 class mockDB {
 	public function query(){}
+	public function exec(){}
 };
 
 class FruitTest extends TestCase {
@@ -20,7 +24,7 @@ class FruitTest extends TestCase {
 
 	# execute setup code before each test is run
 	public function setUp() {
-		$this->db = $this->createMock('modDB');
+		$this->db = $this->createMock('mockDb');
 		$this->app = (new roman\slimApi\App($this->db))->get();
 	}
 
@@ -33,8 +37,9 @@ class FruitTest extends TestCase {
 		$req = Request::createFromEnvironment($env);
 		$this->app->getContainer()['request'] = $req;
 		$response = $this->app->run(true);
-		$this->assertSame($response->getStatusCode(), 200);
-		$this->assertSame((string)$response->getBody(), "hello, roman")
+		$this->assertSame(200, $response->getStatusCode());
+		$this->assertSame((string)$response->getBody(), "hello, roman"),
+		(string)$response->getBody());
 	}
 
 	/*
@@ -49,23 +54,79 @@ class FruitTest extends TestCase {
 
 	# mock the query class & fetchAll functions
 	$query = $this->createMock('mockQuery');
-	$query->metho('fetchAll');
+	$query->method('fetchAll');
 		->willReturn(json_decode($resultString, true)
 	);
 		$this->db->method('query')
 			->willReturn($query);
-	}
 
 	# mock the request environment (part of slim)
 	$env = Environment::mock([
 		'REQUEST_METHOD' => 'GET',
 		'REQUEST_URI'	 => '/fruit',
 	]);
+	$req = Request::createFromEnvironment($env);
+	$this->app->getContainer()['request'] = $req;
 
 	# actually run the request through the app
 	$response = $this->app->run(true);
 
 	# assert expected status code and body
-	$this->assertSame($response->getStatusCode(), 200);
-	$this->assertSame((string)$response->getBody(), $resultedString);
+	$this->assertSame(200, $response->getStatusCode());
+	$this->assertSame($resultString, (string)$response->getBody());
+	}
+
+	public function testGetFruitFailed() {
+		$query = $this->createMock('mockQuery');
+		$query->method('fetch')->willReturn(false);
+		$this->db->method('query')->willReturn($query);
+		$env = Environment::mock([
+			'REQUEST_METHOD' => 'GET',
+			'REQUEST_URI'    => '/fruit/1',
+		]);
+		$req = Request::createFromEnvironment($env);
+		$this->app->getContainer()['request'] = $req;
+
+		# actually run the request through the app
+		$response = $this->app->run(true);
+
+		# assert expected status code and body
+		$this->assertSame(404, $response->getStatusCode());
+		$this->assertSame('{"status":404, "message":"not found"}', (string)$response->getBody());
+	}
+
+	public function testUpdateFruit() {
+		# expected result $resultString
+		$resultString = '{"id":"1", "fruit_name":"apple", "fruit_color":"red", "season":"fall", "calories":"103", "description":"crunchy yet refreshing"}';
+
+		# mock the query class & fetchAll functions
+		$query = $this->createMock('mockQuery');
+		$query->method('fetch')
+			->willReturn(json_decode($resultString, true)
+		);
+		$this->db->method('query')
+			->willReturn($query);
+		 $this->db->method('exec')
+		 	->willReturn(true);
+
+		# mock the request environment (part of slim)
+		$env = Environment::mock([
+			'REQUET_METHOD' => 'PUT',
+			'REQUEST_URI'   => '/fruit/1',
+		]);
+		$req = Request::createFromEnvironment($env);
+		$requestBody = ["fruit_name" => "apple", "fruit_color" => "red", "season" => "fall", "calories" => "103", "description" => "crunchy yet refreshing"];
+		$req = $req->withParsedBody($requestBody);
+		$this->app->getContainer()['request'] = $req;
+
+		# actually run the request through the app
+		$response = $this->app->run(true);
+
+		# assert expected status code and body
+		$this->assertSame(200, $response->getStatusCode());
+		$this->assertSame($resultString, (string)$response->getbody());
+	}
+
+	# test fruit update failed due to invalid fields
+	
 }
